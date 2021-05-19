274687a1e65be439d04e729cf933465958530741
zoeliauh <ibm801227@hotmail.com>
2021-05-20 00:22:18 +0800

    firstTry

---
+++ BullsEyy/AboutViewController.swift
@@ -0,0 +1,20 @@
+//
+//  AboutViewController.swift
+//  BullsEyy
+//
+//  Created by woanjwu liauh on 2021/5/11.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+      super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss (animated: true, completion: nil)
+    }
+
+}
---
+++ BullsEyy/AutherViewController.swift
@@ -0,0 +1,18 @@
+//
+//  AutherViewController.swift
+//  BullsEyy
+//
+//  Created by woanjwu liauh on 2021/5/11.
+//
+
+import UIKit
+
+class AutherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+        @IBAction func close() {
+            dismiss (animated: true, completion: nil)
+        }
+}
